b35813d130c0d9dfa703d67768c09fd1eb5a7bf0
Benoit Blais <benoit.blais@hashicorp.com>
2025-10-24 15:56:46 -0400

    initial release

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-# The following code block is used to create and manage the project where all the workspaces related to the published modules will be stored.
+# The following code block is used to create and manage the project where all the workspaces related to the projects will be stored.
 
 resource "tfe_project" "this" {
   count        = var.project_name != null ? 1 : 0
@@ -22,133 +22,40 @@ resource "tfe_variable_set" "this" {
   parent_project_id = tfe_project.this[0].id
 }
 
-# # The following module blocks are used to create and manage the HCP Terraform teams required by the `modules factory`.
+# The following module blocks are used to create and manage the HCP Terraform teams required by the `projects factory`.
 
-# module "modules_factory_team_hcp" {
-#   source       = "./modules/tfe_team"
-#   count        = length(tfe_project.this) > 0 ? 1 : 0
-#   name         = lower(replace("${tfe_project.this[0].name}-hcp", "/\\W|_|\\s/", "-"))
-#   organization = var.organization_name
-#   organization_access = {
-#     manage_modules = true
-#   }
-#   token = true
-# }
+module "projects_factory_team_hcp" {
+  source       = "app.terraform.io/benoitblais-hashicorp/team/tfe"
+  version      = "1.0.0"
+  count        = length(tfe_project.this) > 0 ? 1 : 0
+  name         = lower(replace("${tfe_project.this[0].name}-hcp", "/\\W|_|\\s/", "-"))
+  organization = var.organization_name
+  organization_access = {
+    manage_membership          = true
+    manage_organization_access = true
+    manage_projects            = true
+    manage_teams               = true
+    manage_workspaces          = true
+  }
+  token = true
+}
 
-# module "modules_factory_team_git" {
-#   source       = "./modules/tfe_team"
-#   count        = length(tfe_project.this) > 0 ? 1 : 0
-#   name         = lower(replace("${tfe_project.this[0].name}-git", "/\\W|_|\\s/", "-"))
-#   organization = var.organization_name
-#   organization_access = {
-#     manage_modules = true
-#   }
-#   token = true
-# }
+# The following resource blocks are used to create variables that will be stored into the variable set previously created.
 
-# # The following resource blocks are used to create variables that will be stored into the variable set previously created.
+resource "tfe_variable" "tfe_token" {
+  count           = length(module.modules_factory_team_hcp) > 0 ? 1 : 0
+  key             = "TFE_TOKEN"
+  value           = module.modules_factory_team_hcp[0].token
+  category        = "env"
+  sensitive       = true
+  variable_set_id = tfe_variable_set.this[0].id
+}
 
-# resource "tfe_variable" "tfe_token" {
-#   count           = length(module.modules_factory_team_hcp) > 0 ? 1 : 0
-#   key             = "TFE_TOKEN"
-#   value           = module.modules_factory_team_hcp[0].token
-#   category        = "env"
-#   sensitive       = true
-#   variable_set_id = tfe_variable_set.this[0].id
-# }
-
-# resource "tfe_variable" "github_app_id" {
-#   count           = length(tfe_variable_set.this) > 0 ? 1 : 0
-#   key             = "GITHUB_APP_ID"
-#   value           = var.app_id
-#   category        = "env"
-#   sensitive       = true
-#   variable_set_id = tfe_variable_set.this[0].id
-# }
-
-# resource "tfe_variable" "github_app_installation_id" {
-#   count           = length(tfe_variable_set.this) > 0 ? 1 : 0
-#   key             = "GITHUB_APP_INSTALLATION_ID"
-#   value           = var.app_installation_id
-#   category        = "env"
-#   sensitive       = true
-#   variable_set_id = tfe_variable_set.this[0].id
-# }
-
-# resource "tfe_variable" "github_app_pem_file" {
-#   count           = length(tfe_variable_set.this) > 0 ? 1 : 0
-#   key             = "GITHUB_APP_PEM_FILE"
-#   value           = var.app_pem_file
-#   category        = "env"
-#   sensitive       = true
-#   variable_set_id = tfe_variable_set.this[0].id
-# }
-
-# resource "tfe_variable" "github_owner" {
-#   count           = length(tfe_variable_set.this) > 0 ? 1 : 0
-#   key             = "GITHUB_OWNER"
-#   value           = var.github_organization
-#   category        = "env"
-#   sensitive       = true
-#   variable_set_id = tfe_variable_set.this[0].id
-# }
-
-# resource "tfe_variable" "oauth_client_name" {
-#   count           = length(tfe_variable_set.this) > 0 ? 1 : 0
-#   key             = "oauth_client_name"
-#   value           = var.oauth_client_name
-#   category        = "terraform"
-#   description     = "(Optional) Name of the OAuth client."
-#   variable_set_id = tfe_variable_set.this[0].id
-# }
-
-# resource "tfe_variable" "organization" {
-#   count           = length(tfe_variable_set.this) > 0 ? 1 : 0
-#   key             = "organization"
-#   value           = var.organization_name
-#   category        = "terraform"
-#   description     = "(Optional) A description for the project."
-#   variable_set_id = tfe_variable_set.this[0].id
-# }
-
-# locals {
-#   github_teams = [for team in var.github_teams :
-#     {
-#       name       = team.name
-#       permission = team.permission
-#     }
-#   ]
-#   github_teams_formated = [for team in local.github_teams :
-#     "  {\n    name = \"${team.name}\",\n    permission = \"${team.permission}\"\n  }"
-#   ]
-#   github_teams_string = "[\n${join(",\n", local.github_teams_formated)}\n]"
-# }
-
-# resource "tfe_variable" "github_teams" {
-#   count           = length(tfe_variable_set.this) > 0 ? 1 : 0
-#   key             = "github_teams"
-#   value           = local.github_teams_string
-#   category        = "terraform"
-#   description     = "(Optional) The github_teams block supports the following:\nname: (Required) The name of the team.\npermission: (Optional) The permissions of team members regarding the repository. Must be one of `pull`, `triage`, `push`, `maintain`, `admin` or the name of an existing custom repository role within the organisation."
-#   hcl             = true
-#   variable_set_id = tfe_variable_set.this[0].id
-# }
-
-# resource "tfe_variable" "template" {
-#   count           = length(tfe_variable_set.this) > 0 && var.github_template != null ? 1 : 0
-#   key             = "template"
-#   value           = "{\n  owner = \"${var.github_organization}\",\n  repository = \"${var.github_template}\"\n}"
-#   category        = "terraform"
-#   description     = "(Optional) The template block supports the following:\nowner: (Required) The GitHub organization or user the template repository is owned by.\nrepository: (Required) The name of the template repository."
-#   hcl             = true
-#   variable_set_id = tfe_variable_set.this[0].id
-# }
-
-# resource "tfe_variable" "git_tfe_token" {
-#   count           = length(tfe_variable_set.this) > 0 ? 1 : 0
-#   key             = "tfe_token"
-#   value           = var.organization_name
-#   category        = "terraform"
-#   description     = "(Optional) The TFE_TOKEN secret value to be created in the GitHub repository to allow the module to publish itself into the private registry."
-#   variable_set_id = tfe_variable_set.this[0].id
-# }
+resource "tfe_variable" "organization" {
+  count           = length(tfe_variable_set.this) > 0 ? 1 : 0
+  key             = "organization"
+  value           = var.organization_name
+  category        = "terraform"
+  description     = "(Optional) A description for the project."
+  variable_set_id = tfe_variable_set.this[0].id
+}
--- variables.tf
+++ variables.tf
@@ -4,51 +4,6 @@ variable "organization_name" {
   nullable    = false
 }
 
-variable "github_teams" {
-  description = <<EOT
-  (Optional) The github_teams block supports the following:
-    name        : (Required) The name of the team.
-    description : (Optional) A description of the team.
-    permission  : (Optional) The permissions of team members regarding the repository. Must be one of `pull`, `triage`, `push`, `maintain`, `admin` or the name of an existing custom repository role within the organisation.
-  EOT
-  type = list(object({
-    name        = string
-    description = optional(string)
-    permission  = optional(string, "pull")
-  }))
-  nullable = false
-  default = [{
-    name        = "Terraform-Projects-Owners"
-    description = "This group grant admin access to the Terraform Projects repository."
-    permission  = "admin"
-    },
-    {
-      name        = "Terraform-Projects-Contributors"
-      description = "This group grant write access to the Terraform Projects repository."
-      permission  = "push"
-  }]
-}
-
-variable "github_template" {
-  description = "(Optional) The GitHub repository to use as a template when creating new repositories. The repository must be a template repository. If not provided, the default template provided by the Project will be used."
-  type        = string
-  nullable    = true
-  default     = null
-}
-
-variable "Project_name" {
-  description = "(Optional) Name of the terraform Project used by the Projects factory."
-  type        = string
-  default     = "terraform-tfe-Projectsfactory"
-}
-
-variable "oauth_client_name" {
-  description = "(Optional) Name of the OAuth client."
-  type        = string
-  nullable    = false
-  default     = "GitHub"
-}
-
 variable "project_description" {
   description = "(Optional) A description for the project."
   type        = string
